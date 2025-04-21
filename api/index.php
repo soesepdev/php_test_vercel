657eccc9d036eea3c30b10392886e66334aa4fe0
Soesep Dev <soesep.dev@gmail.com>
2025-04-21 22:51:31 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,3 +2,12 @@
 
 define('BASE_PATH', __DIR__);
 include "config/route.php";
+
+use MongoDB\Client;
+
+try {
+    $mongoClient = new Client("mongodb+srv://soesepdev:ORN3ZV3IYeIYsZNf@cluster0.nj1riyy.mongodb.net/");
+    $db = $mongoClient->selectDatabase("portfolio"); // Ganti dengan nama database kamu
+} catch (Exception $e) {
+    die("MongoDB Connection Error: " . $e->getMessage());
+}
