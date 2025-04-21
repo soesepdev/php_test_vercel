c962c27034fce0d31d4bda404f0f617bb7fa2db7
Soesep Dev <soesep.dev@gmail.com>
2025-04-21 23:25:02 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,11 +3,11 @@
 // define('BASE_PATH', __DIR__);
 // include "config/route.php";
 
-$host = getenv('PGHOST') ?: 'aws-0-ap-southeast-1.pooler.supabase.com';
-$port = getenv('PGPORT') ?: '6543';
-$dbname = getenv('PGDATABASE') ?: 'postgres';
-$user = getenv('PGUSER') ?: 'postgres.zbnysbkzjcidikfgmqsm';
-$password = getenv('PGPASSWORD') ?: 'SoesepDev2025';
+$host = getenv('PGHOST');
+$port = getenv('PGPORT');
+$dbname = getenv('PGDATABASE');
+$user = getenv('PGUSER');
+$password = getenv('PGPASSWORD');
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
