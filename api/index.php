3c092d47aa934b5bf2594fa1e78d14cf9bfcb112
Soesep Dev <soesep.dev@gmail.com>
2025-04-21 16:47:12 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,14 +1,19 @@
 <?php
 error_reporting(0);
 
-$path = $_SERVER['REQUEST_URI'];
-$segments = explode('/', trim($path, '/')); 
+// Ambil path URI tanpa query string
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if (isset($segments[0])) {
-    $route = $segments[0];
+// Pecah menjadi segmen-segmen
+$segments = explode('/', trim($path, '/'));
+
+// Gabungkan semua segmen menjadi satu string
+$route = implode('/', $segments);
+
+// Tampilkan route jika ada
+if (!empty($route)) {
     echo $route;
 } else {
     phpinfo();
 }
-    
 ?>
