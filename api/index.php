5c12fc5d21c8ff59f6695f780fbad7f11aa3f133
Soesep Dev <soesep.dev@gmail.com>
2025-04-21 23:05:40 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,4 +3,18 @@
 // define('BASE_PATH', __DIR__);
 // include "config/route.php";
 
-$client = new MongoDB\Client("mongodb+srv://soesepdev:ORN3ZV3IYeIYsZNf@cluster0.nj1riyy.mongodb.net/");
+<?php
+
+$host = "localhost";       // Contoh: 127.0.0.1 atau db-host.example.com
+$user = "root";            // Contoh: root
+$pass = "";            // Contoh: secret
+$dbname = "test";     // Contoh: myapp
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully!";
