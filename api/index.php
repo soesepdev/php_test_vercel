ccfe92caad4d4057e1fa179888dce7776caaef51
Soesep Dev <soesep.dev@gmail.com>
2025-04-21 22:10:02 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,24 +1,3 @@
 <?php
-//error_reporting(0);
 
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$segments = explode('/', trim($path, '/'));
-$route = implode('/', $segments);
-
-var_dump($segments);
-
-if (!empty($route)) {
-    $dir = __DIR__ . "/$route";
-    if(file_exists("$dir.php")) {
-        include "$route.php";
-    } else {
-        if(file_exists("$dir/index.php")) {
-            include "$route/index.php";
-        } else {
-            phpinfo();
-        }
-    }
-} else {
-    phpinfo();
-}
-?>
+include "config/route.php";
