a0de5f9bd8adf1331e49d6b6a5f1fdb6424e531e
Soesep Dev <soesep.dev@gmail.com>
2025-04-21 22:57:50 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,9 +2,6 @@
 
 // define('BASE_PATH', __DIR__);
 // include "config/route.php";
-
-use MongoDB\Client;
-
 try {
     $mongoClient = new Client("mongodb+srv://soesepdev:ORN3ZV3IYeIYsZNf@cluster0.nj1riyy.mongodb.net/");
     $db = $mongoClient->selectDatabase("portfolio"); // Ganti dengan nama database kamu
