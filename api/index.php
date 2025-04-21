edb9bca24a68c9b38c3944f6d4886aa54632cbda
Soesep Dev <soesep.dev@gmail.com>
2025-04-21 16:32:44 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@ error_reporting(0);
 $path = $_SERVER['REQUEST_URI'];
 $segments = explode('/', trim($path, '/')); 
 
-if (isset($segments[1])) {
+if (isset($segments[0])) {
     $route = $segments[0];
     echo $route;
 } else {
