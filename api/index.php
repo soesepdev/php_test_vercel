273796a5354a85917126798d60bee080f5a0bca8
Soesep Dev <soesep.dev@gmail.com>
2025-04-22 17:06:53 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,24 +2,6 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// define('BASE_PATH', __DIR__);
-// include "config/route.php";
-
-
-use MongoDB\Client;
-
-try {
-    // Ganti dengan Mongo URI yang sesuai
-    $client = new Client("mongodb+srv://soesepdev:SoesepDev2025@cluster0.nj1riyy.mongodb.net/");
-    $database = $client->selectDatabase('portfolio');
-
-    $collection = $database->selectCollection('users');
-    $cursor = $collection->find();
-
-    foreach ($cursor as $value) {
-        echo $value->name;
-        echo "</br>";
-    }
-} catch (Exception $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
+define('BASE_PATH', __DIR__);
+include "config/database.php";
+include "config/route.php";
