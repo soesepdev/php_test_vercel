782d26f0a5f24c5ab0a1dc29cdf5d9fa16ce4d02
Soesep Dev <soesep.dev@gmail.com>
2025-04-22 17:10:38 +0700

    Update index.php

--- api/modules/users/index.php
+++ api/modules/users/index.php
@@ -1,14 +1,22 @@
 <?php
-header('Content-Type: application/json');
+// header('Content-Type: application/json');
 
-$response = [
-    "status" => "success",
-    "message" => "Users",
-    "data" => [
-        "id" => 1,
-        "name" => "Soesep Dev",
-        "gender" => "Male"
-    ]
-];
+// $response = [
+//     "status" => "success",
+//     "message" => "Users",
+//     "data" => [
+//         "id" => 1,
+//         "name" => "Soesep Dev",
+//         "gender" => "Male"
+//     ]
+// ];
 
-echo json_encode($response);
+// echo json_encode($response);
+
+    $collection = $database->selectCollection('users');
+    $cursor = $collection->find();
+
+    foreach ($cursor as $value) {
+        echo $value->name;
+        echo "</br>";
+    }
