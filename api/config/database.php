b51d095bb8296962292e7bef658a5dc3c3efc9e6
Soesep Dev <soesep.dev@gmail.com>
2025-04-22 17:06:19 +0700

    Update database.php

--- api/config/database.php
+++ api/config/database.php
@@ -1,1 +1,18 @@
+<?php
 
+use MongoDB\Client;
+
+try {
+    $client = new Client("mongodb+srv://soesepdev:SoesepDev2025@cluster0.nj1riyy.mongodb.net/");
+    $database = $client->selectDatabase('portfolio');
+
+    $collection = $database->selectCollection('users');
+    $cursor = $collection->find();
+
+    foreach ($cursor as $value) {
+        echo $value->name;
+        echo "</br>";
+    }
+} catch (Exception $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
