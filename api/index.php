728faf089cfbbdcd394d0002c272a83a15c381ed
Soesep Dev <soesep.dev@gmail.com>
2025-04-22 16:52:44 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -10,9 +10,10 @@ use MongoDB\Client;
 
 try {
     // Ganti dengan Mongo URI yang sesuai
-    $client = new Client("mongodb://localhost:27017");
-    $database = $client->selectDatabase('test');
+    $client = new Client("mongodb+srv://soesepdev:SoesepDev2025@cluster0.nj1riyy.mongodb.net/");
+    $database = $client->selectDatabase('portfolio');
     echo "MongoDB connection successful!";
+    var_dump($client);
 } catch (Exception $e) {
     echo "Connection failed: " . $e->getMessage();
 }
